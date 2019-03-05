59fd6dcfaf58e9b541423bf289a0ddec2feb543a
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-03-04 23:58:23 -0600

    Bresenham
     Mejores y correcciones en setPixelCicle y termino de bresenham
     Tambien se le agrego el timer para medir los tiempos

--- draw-cricle/FormMain.cs
+++ draw-cricle/FormMain.cs
@@ -7,21 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+// Add for use Timer
+using System.Diagnostics;
 
 namespace draw_cricle
 {
     public partial class FormMain : Form
     {
-        Point centro, radio;
-        Bitmap bmp;
         const int width = 500;
         const int height = 500;
+        Point centro, radio;
+        Bitmap bmp;
+        Stopwatch sw;
 
         public FormMain()
         {
             centro = new Point(-1, -1);
             radio = new Point(-1, -1);
             bmp = new Bitmap(width, height);
+            sw = new Stopwatch();
             InitializeComponent();
         }
 
@@ -58,102 +62,101 @@ namespace draw_cricle
                 Console.WriteLine("punto inicial: (" + centro.X + ", " + centro.Y + ")");
                 Console.WriteLine("punto final: (" + radio.X + ", " + radio.Y + ")");
 
+                sw.Restart();
                 DDA(centro, radio);
-                //bresenham(initial, final);
+                labelAdd.Text = "ADD: " + String.Format("{0}", sw.Elapsed.TotalMilliseconds) + " s";
+
+                sw.Restart();
+                bresenham(centro, radio);
+                labelBresenham.Text = "Bresenham: " + String.Format("{0}", sw.Elapsed.TotalMilliseconds) + " s";
+
                 centro.X = centro.Y = -1;
                 radio.X = radio.Y = -1;
             }
         }
 
-        // function to draw all other 7 pixels 
-        // present at symmetric position 
+        /*
+         * Funcion de dibujo de pixeles simetricos
+         * en los 8 octantes del circulo.
+         * Con verificación de existencia
+         */
         private void setPixelCircle(int xc, int yc, int x, int y, Color color)
         {
-            if((xc + x) <= pictureBox1.Width && (xc + x) >= 0)
+            if((xc + x) < pictureBox1.Width && (xc + x) >= 0)
             {
-                if((yc + y) <= pictureBox1.Height && (yc + y) >= 0)
+                if((yc + y) < pictureBox1.Height && (yc + y) >= 0)
                 {
                     bmp.SetPixel(xc + x, yc + y, color);
                 }
             }
-            if ((xc - x) <= pictureBox1.Width && (xc - x) >= 0)
+            if ((xc - x) < pictureBox1.Width && (xc - x) >= 0)
             {
-                if ((yc + y) <= pictureBox1.Height && (yc + y) >= 0)
+                if ((yc + y) < pictureBox1.Height && (yc + y) >= 0)
                 {
                     bmp.SetPixel(xc - x, yc + y, color);
                 }
             }
-            if ((xc + x) <= pictureBox1.Width && (xc + x) >= 0)
+            if ((xc + x) < pictureBox1.Width && (xc + x) >= 0)
             {
-                if ((yc - y) <= pictureBox1.Height && (yc - y) >= 0)
+                if ((yc - y) < pictureBox1.Height && (yc - y) >= 0)
                 {
                     bmp.SetPixel(xc + x, yc - y, color);
                 }
             }
-            if ((xc - x) <= pictureBox1.Width && (xc - x) >= 0)
+            if ((xc - x) < pictureBox1.Width && (xc - x) >= 0)
             {
-                if ((yc - y) <= pictureBox1.Height && (yc - y) >= 0)
+                if ((yc - y) < pictureBox1.Height && (yc - y) >= 0)
                 {
                     bmp.SetPixel(xc - x, yc - y, color);
                 }
             }
-            if ((xc + y) <= pictureBox1.Width && (xc + y) >= 0)
+            if ((xc + y) < pictureBox1.Width && (xc + y) >= 0)
             {
-                if ((yc + x) <= pictureBox1.Height && (yc + x) >= 0)
+                if ((yc + x) < pictureBox1.Height && (yc + x) >= 0)
                 {
                     bmp.SetPixel(xc + y, yc + x, color);
                 }
             }
-            if ((xc - y) <= pictureBox1.Width && (xc - y) >= 0)
+            if ((xc - y) < pictureBox1.Width && (xc - y) >= 0)
             {
-                if ((yc + x) <= pictureBox1.Height && (yc + x) >= 0)
+                if ((yc + x) < pictureBox1.Height && (yc + x) >= 0)
                 {
                     bmp.SetPixel(xc - y, yc + x, color);
                 }
             }
-            if ((xc + y) <= pictureBox1.Width && (xc + y) >= 0)
+            if ((xc + y) < pictureBox1.Width && (xc + y) >= 0)
             {
-                if ((yc - x) <= pictureBox1.Height && (yc - x) >= 0)
+                if ((yc - x) < pictureBox1.Height && (yc - x) >= 0)
                 {
                     bmp.SetPixel(xc + y, yc - x, color);
                 }
             }
-            if ((xc - y) <= pictureBox1.Width && (xc - y) >= 0)
+            if ((xc - y) < pictureBox1.Width && (xc - y) >= 0)
             {
-                if ((yc - x) <= pictureBox1.Height && (yc - x) >= 0)
+                if ((yc - x) < pictureBox1.Height && (yc - x) >= 0)
                 {
                     bmp.SetPixel(xc - y, yc - x, color);
                 }
             }
-            //bmp.SetPixel(xc + x, yc + y, color);
-            //bmp.SetPixel(xc - x, yc + y, color);
-            //bmp.SetPixel(xc + x, yc - y, color);
-            //bmp.SetPixel(xc - x, yc - y, color);
-            //bmp.SetPixel(xc + y, yc + x, color);
-            //bmp.SetPixel(xc - y, yc + x, color);
-            //bmp.SetPixel(xc + y, yc - x, color);
-            //bmp.SetPixel(xc - y, yc - x, color);
         }
 
+        /*
+         * Algoritmo de trasado de circunferencia con DDA
+         * yk = Raiz( r^2 - xk^2)
+         * 
+         */
         private void DDA(Point centro, Point radio)
         {
+            Stopwatch sw = new Stopwatch();
             double r = 0;
             double yk = 0;
             int xk = 0;
-            int xc = 0;
-            int yc = 0;
-            int xf = 0;
-            int yf = 0;
-            int a = 0;
-            int b = 0;
-
-            xc = centro.X;
-            yc = centro.Y;
-            xf = radio.X;
-            yf = radio.Y;
-
-            a = xf - xc;
-            b = yf - yc;
+            int xc = centro.X;
+            int yc = centro.Y;
+            int xf = radio.X;
+            int yf = radio.Y;
+            int a = xf - xc;
+            int b = yf - yc;
 
             r = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
             r = Math.Round(r);
@@ -173,9 +176,40 @@ namespace draw_cricle
             pictureBox1.Image = bmp;
         }
 
+        /*
+         * Algoritmo de trasado de circunferencia con Bresenham o MidPoint
+         */
         private void bresenham(Point centro, Point radio)
         {
+            Stopwatch sw = new Stopwatch();
+            double r = 0;
+            int xc = centro.X;
+            int yc = centro.Y;
+            int xf = radio.X;
+            int yf = radio.Y;
+            int a = xf - xc;
+            int b = yf - yc;
+            int x = 0, y = 0, e = 0;
 
+            
+            r = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            r = Math.Round(r);
+            r = Math.Abs(r);
+
+            x = (int)r;
+            while(y <= x)
+            {
+                setPixelCircle(xc, yc, x, y, Color.Blue);
+                e = e + (2 * y) + 1;
+                y = y + 1;
+                if((2 * e) > ((2 * x) - 1))
+                {
+                    x = x - 1;
+                    e = e - (2 * x) + 1;
+                }
+            }
+
+            pictureBox1.Image = bmp;
         }
     }
 }
